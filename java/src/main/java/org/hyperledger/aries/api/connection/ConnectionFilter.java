f94cf9b4853742e7e1d41e5af3bfc21c53c3cdc0
Philipp Etschel <philipp.etschel@ch.bosch.com>
2021-04-07 18:02:22 +0200

    generic request filter, to get rid of a lot of boilerplate code.
    Signed-off-by: Philipp Etschel <philipp.etschel@ch.bosch.com>

--- java/src/main/java/org/hyperledger/aries/api/connection/ConnectionFilter.java
+++ java/src/main/java/org/hyperledger/aries/api/connection/ConnectionFilter.java
@@ -7,16 +7,12 @@ package org.hyperledger.aries.api.connection;
 
 import lombok.Builder;
 import lombok.Data;
-import lombok.NonNull;
-import okhttp3.HttpUrl;
-import org.apache.commons.lang3.StringUtils;
+import org.hyperledger.aries.api.AcaPyRequestFilter;
 
 import javax.annotation.Nullable;
-import java.util.Locale;
 
 @Data @Builder
-public class ConnectionFilter {
-
+public class ConnectionFilter implements AcaPyRequestFilter {
     @Nullable private String alias;
     @Nullable private ConnectionInitiator initiator;
     @Nullable private String invitationKey;
@@ -24,29 +20,4 @@ public class ConnectionFilter {
     @Nullable private ConnectionState state;
     @Nullable private String theirDid;
     @Nullable private String theirRole;
-
-    public HttpUrl.Builder buildParams(@NonNull HttpUrl.Builder b) {
-        if (StringUtils.isNotEmpty(alias)) {
-            b.addQueryParameter("alias", alias);
-        }
-        if (initiator != null) {
-            b.addQueryParameter("initiator", initiator.toString());
-        }
-        if (StringUtils.isNotEmpty(invitationKey)) {
-            b.addQueryParameter("invitation_key", invitationKey);
-        }
-        if (StringUtils.isNotEmpty(myDid)) {
-            b.addQueryParameter("my_did", myDid);
-        }
-        if (state != null) {
-            b.addQueryParameter("state", state.toString().toLowerCase(Locale.US));
-        }
-        if (StringUtils.isNotEmpty(theirDid)) {
-            b.addQueryParameter("their_did", theirDid);
-        }
-        if (StringUtils.isNotEmpty(theirRole)) {
-            b.addQueryParameter("their_role", theirRole);
-        }
-        return b;
-    }
 }
