6841db5522406211ab4826f3b3ec953e8f92962a
Philipp Etschel <philipp.etschel@ch.bosch.com>
2021-05-18 11:40:38 +0200

    latest 0.7.0-pre1 changes in the generated models
    Signed-off-by: Philipp Etschel <philipp.etschel@ch.bosch.com>

--- java/src/main/java/org/hyperledger/acy_py/generated/model/PresentationPreview.java
+++ java/src/main/java/org/hyperledger/acy_py/generated/model/PresentationPreview.java
@@ -12,20 +12,10 @@
 
 package org.hyperledger.acy_py.generated.model;
 
-import java.util.Objects;
-import java.util.Arrays;
-import com.google.gson.TypeAdapter;
-import com.google.gson.annotations.JsonAdapter;
 import com.google.gson.annotations.SerializedName;
-import com.google.gson.stream.JsonReader;
-import com.google.gson.stream.JsonWriter;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-import java.io.IOException;
+
 import java.util.ArrayList;
 import java.util.List;
-import org.hyperledger.acy_py.generated.model.PresAttrSpec;
-import org.hyperledger.acy_py.generated.model.PresPredSpec;
 
 /**
  * PresentationPreview
