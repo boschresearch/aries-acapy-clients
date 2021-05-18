d2bf4d0537a78f6b2e784d0a0d9244e8192f219a
Philipp Etschel <philipp.etschel@ch.bosch.com>
2021-05-18 09:59:48 +0200

    latest pre version
    Signed-off-by: Philipp Etschel <philipp.etschel@ch.bosch.com>

--- java/src/test/java/org/hyperledger/aries/IntegrationTestBase.java
+++ java/src/test/java/org/hyperledger/aries/IntegrationTestBase.java
@@ -19,7 +19,7 @@ public abstract class IntegrationTestBase {
 
     private final Logger log = LoggerFactory.getLogger(IntegrationTestBase.class);
 
-    public static final String ARIES_VERSION = "bcgovimages/aries-cloudagent:py36-1.16-0_0.7-pre.0";
+    public static final String ARIES_VERSION = "bcgovimages/aries-cloudagent:py36-1.16-0_0.7-pre.1";
     public static final Integer ARIES_ADMIN_PORT = 8031;
 
     protected AriesClient ac;
