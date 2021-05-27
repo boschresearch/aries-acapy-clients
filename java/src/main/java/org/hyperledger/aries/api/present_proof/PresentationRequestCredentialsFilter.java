b317cb7eb3552c118f9053f6e585123b438327e3
Philipp Etschel <philipp.etschel@ch.bosch.com>
2021-05-27 16:43:08 +0200

    updating license headers
    Signed-off-by: Philipp Etschel <philipp.etschel@ch.bosch.com>

--- java/src/main/java/org/hyperledger/aries/api/present_proof/PresentationRequestCredentialsFilter.java
+++ java/src/main/java/org/hyperledger/aries/api/present_proof/PresentationRequestCredentialsFilter.java
@@ -1,5 +1,7 @@
 /*
- * Copyright (c) 2020-2021 Robert Bosch GmbH. All Rights Reserved.
+ * Copyright (c) 2020-2021 - for information on the respective copyright owner
+ * see the NOTICE file and/or the repository at
+ * https://github.com/hyperledger-labs/acapy-java-client
  *
  * SPDX-License-Identifier: Apache-2.0
  */
